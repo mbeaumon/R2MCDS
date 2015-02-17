aa7b1507fc52acdeade08c036f71098a6c588437
RoyChristian <christian.roy.79@gmail.com>
2015-02-17 15:00:54 -0500

    starting version 0.7.2

--- R/GeoAviR-package.r
+++ R/GeoAviR-package.r
@@ -8,8 +8,8 @@
 #'\tabular{ll}{
 #'Package: \tab GeoAviR\cr
 #'Type: \tab Package\cr
-#'Version: \tab 0.7.1\cr
-#'Date: \tab 2015-01-09\cr
+#'Version: \tab 0.7.2\cr
+#'Date: \tab 2015-02-17\cr
 #'License: \tab GPL-2\cr
 #'LazyLoad: \tab yes\cr
 #'}
