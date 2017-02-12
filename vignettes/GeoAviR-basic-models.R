04267eb2f14ab943a58984eac47e07922afee643
Roy <Christian.Roy@ec.gc.ca>
2017-02-11 19:02:17 -0500

    created vignettes documents

---
+++ vignettes/GeoAviR-basic-models.R
@@ -0,0 +1,85 @@
+## ----warning=FALSE,message=FALSE-----------------------------------------
+library(GeoAviR)
+### Import and filter data
+data(alcidae)
+alcids<-filterECSAS(alcidae)
+
+### Run analysis with the MCDS engine. Here, the WatchID is used as the sample.
+x<-distance.wrap(alcids, SMP_EFFORT="WatchLenKm",DISTANCE="Distance",SIZE="Count",
+                 units=list(Type="Line",Distance="Perp",Length_units="Kilometers",
+                            Distance_units="Meters",Area_units="Square kilometers"),
+                 breaks=c(0,50,100,200,300), STR_LABEL="STR_LABEL", 
+                 STR_AREA="STR_AREA",SMP_LABEL="WatchID", 
+                 path="c:/temp/distance",
+                 pathMCDS="C:/Distance 6",verbose=FALSE)
+
+## ------------------------------------------------------------------------
+x
+####summary of one of the best model
+summary(x[[5]])
+
+## ------------------------------------------------------------------------
+##### Keep the 'best' model in the list
+x.best <- keep.best.model(x)
+summary(x.best)
+
+## ----warning=FALSE,message=FALSE-----------------------------------------
+library(GeoAviR)
+### Import and filter data
+data(alcidae)
+alcids<-filterECSAS(alcidae)
+
+### Run analysis with the MCDS engine. Here, the WatchID is used as the sample.
+x<-distance.wrap(alcids,SMP_EFFORT="WatchLenKm",DISTANCE="Distance",SIZE="Count",
+                 units=list(Type="Line",Distance="Perp",Length_units="Kilometers",
+                            Distance_units="Meters",Area_units="Square kilometers"),
+                 breaks=c(0,50,100,200,300), SMP_LABEL="WatchID",
+                 STR_LABEL="STR_LABEL",STR_AREA="STR_AREA",
+                 estimator=list(c("HN","CO"),c("HA","PO")),
+                 path="c:/temp/distance",
+                 pathMCDS="C:/Distance 6",verbose=FALSE)
+x
+summary(x[[2]])
+
+## ----warning=FALSE,message=FALSE-----------------------------------------
+library(GeoAviR)
+
+### Import and filter data
+data(quebec)
+d<-filterECSAS(quebec)
+
+### Run analysis with the MCDS engine. Here, the WatchID is used as the sample.
+x<-distance.wrap(d,SMP_EFFORT="WatchLenKm",DISTANCE="Distance",SIZE="Count",
+                 units=list(Type="Line",Distance="Perp",Length_units="Kilometers",
+                            Distance_units="Meters",Area_units="Square kilometers"),
+                 breaks=c(0,50,100,200,300), estimator=list(c("HN","CO")),
+                 lsub=list(Alpha=c("BLKI","GBBG", "HERG")), split=TRUE,
+                 STR_LABEL="STR_LABEL",STR_AREA="STR_AREA",SMP_LABEL="WatchID",
+                 path="c:/temp/distance",
+                 pathMCDS="C:/Distance 6",verbose=FALSE)
+x
+##output for the Herring gull
+summary(x[[3]])
+
+
+## ----warning=FALSE,message=FALSE-----------------------------------------
+#'### 
+library(GeoAviR)
+
+### Import and filter data
+data(quebec)
+d<-filterECSAS(quebec)
+
+### Run analysis with the MCDS engine. Here, the WatchID is used as the sample.
+x<-distance.wrap(d,SMP_EFFORT="WatchLenKm",DISTANCE="Distance",SIZE="Count",
+                 units=list(Type="Line",Distance="Perp",Length_units="Kilometers",
+                            Distance_units="Meters",Area_units="Square kilometers"),
+                 breaks=c(0,50,100,200,300), estimator=list(c("HN","CO")),
+                 lsub=list(Alpha=c("HERG")), rare= list(Alpha=c("RBGU")), split=TRUE,
+                 STR_LABEL="STR_LABEL",STR_AREA="STR_AREA",SMP_LABEL="WatchID",
+                 path="c:/temp/distance",
+                 pathMCDS="C:/Distance 6",verbose=FALSE)
+x
+##output for the Ring-billed Gull
+summary(x)
+
---
+++ vignettes/GeoAviR-covariables-models.R
@@ -0,0 +1,25 @@
+## ----warning=FALSE,message=FALSE-----------------------------------------
+library(GeoAviR)
+###set seed for reproductibility    
+set.seed(062)
+### Import and filter data
+data(alcidae)
+alcids<-filterECSAS(alcidae)
+
+### Run analysis with the MCDS engine. Here, the WatchID is used as the sample.
+x<-distance.wrap(alcids,SMP_EFFORT="WatchLenKm",DISTANCE="Distance",SIZE="Count",
+                 units=list(Type="Line",Distance="Perp",Length_units="Kilometers",
+                            Distance_units="Meters",Area_units="Square kilometers"),
+                 factor = c("Observer1"), monotone = "none",
+                 breaks=c(0,50,100,200,300), SMP_LABEL="WatchID",
+                 STR_LABEL="STR_LABEL",STR_AREA="STR_AREA",
+                 path="c:/temp/distance",
+                 pathMCDS="C:/Distance 6",verbose=FALSE)
+#Look at the output
+x
+
+## ------------------------------------------------------------------------
+#Keep only the best model basec on AICc and look at the output
+x.best <- keep.best.model(x)
+summary(x.best)
+
