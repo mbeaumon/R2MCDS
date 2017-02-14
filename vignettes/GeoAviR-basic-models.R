435b6d35731daca043106ed2ab2f7fd4de6515d6
Roy <Christian.Roy@ec.gc.ca>
2017-02-14 10:30:57 -0500

    updated the basic_model vignette

--- vignettes/GeoAviR-basic-models.R
+++ vignettes/GeoAviR-basic-models.R
@@ -2,9 +2,11 @@
 library(GeoAviR)
 ### Import and filter data
 data(alcidae)
-alcids<-distance.filter(alcidae, transect.id = "WatchID", distance.field = "Distance", distance.labels = c("A", "B", "C", "D"), 
-                        distance.midpoints = c(25, 75, 150, 250), effort.field = "WatchLenKm", lat.field = "LatStart", 
-                        long.field = "LongStart", sp.field = "Alpha", date.field = "Date")
+alcids<-distance.filter(alcidae, transect.id = "WatchID", distance.field = "Distance", 
+                        distance.labels = c("A", "B", "C", "D"), 
+                        distance.midpoints = c(25, 75, 150, 250), effort.field = "WatchLenKm", 
+                        lat.field = "LatStart", long.field = "LongStart", 
+                        sp.field = "Alpha", date.field = "Date")
 
 ### Run analysis with the MCDS engine. Here, the WatchID is used as the sample.
 x<-distance.wrap(alcids, SMP_EFFORT="WatchLenKm",DISTANCE="Distance",SIZE="Count",
@@ -29,9 +31,11 @@ summary(x.best)
 library(GeoAviR)
 ### Import and filter data
 data(alcidae)
-alcids <- distance.filter(alcidae, transect.id = "WatchID", distance.field = "Distance", distance.labels = c("A", "B", "C", "D"), 
-                          distance.midpoints = c(25, 75, 150, 250), effort.field = "WatchLenKm", lat.field = "LatStart", 
-                          long.field = "LongStart", sp.field = "Alpha", date.field = "Date")
+alcids<-distance.filter(alcidae, transect.id = "WatchID", distance.field = "Distance", 
+                        distance.labels = c("A", "B", "C", "D"), 
+                        distance.midpoints = c(25, 75, 150, 250), effort.field = "WatchLenKm", 
+                        lat.field = "LatStart", long.field = "LongStart", 
+                        sp.field = "Alpha", date.field = "Date")
 
 ### Run analysis with the MCDS engine. Here, the WatchID is used as the sample.
 x<-distance.wrap(alcids,SMP_EFFORT="WatchLenKm",DISTANCE="Distance",SIZE="Count",
@@ -46,48 +50,10 @@ x
 summary(x[[2]])
 
 ## ----warning=FALSE,message=FALSE-----------------------------------------
-library(GeoAviR)
+##### Keep the 'best' model in the list
+x.best <- keep.best.model(x)
+summary(x.best)
 
-### Import and filter data
-data(quebec)
-d <- distance.filter(quebec, transect.id = "WatchID", distance.field = "Distance", distance.labels = c("A", "B", "C", "D"), 
-                     distance.midpoints = c(25, 75, 150, 250), effort.field = "WatchLenKm", lat.field = "LatStart", 
-                     long.field = "LongStart", sp.field = "Alpha", date.field = "Date")
+#predicted_hist(x.best)
 
-### Run analysis with the MCDS engine. Here, the WatchID is used as the sample.
-x<-distance.wrap(d,SMP_EFFORT="WatchLenKm",DISTANCE="Distance",SIZE="Count",
-                 units=list(Type="Line",Distance="Perp",Length_units="Kilometers",
-                            Distance_units="Meters",Area_units="Square kilometers"),
-                 breaks=c(0,50,100,200,300), estimator=list(c("HN","CO")),
-                 lsub=list(Alpha=c("BLKI","GBBG", "HERG")), split=TRUE,
-                 STR_LABEL="STR_LABEL",STR_AREA="STR_AREA",SMP_LABEL="WatchID",
-                 path="c:/temp/distance",
-                 pathMCDS="C:/Distance 6",verbose=FALSE)
-x
-##output for the Herring gull
-summary(x[[3]])
-
-
-## ----warning=FALSE,message=FALSE-----------------------------------------
-#'### 
-library(GeoAviR)
-
-### Import and filter data
-data(quebec)
-d <- distance.filter(quebec, transect.id = "WatchID", distance.field = "Distance", distance.labels = c("A", "B", "C", "D"), 
-                     distance.midpoints = c(25, 75, 150, 250), effort.field = "WatchLenKm", lat.field = "LatStart", 
-                     long.field = "LongStart", sp.field = "Alpha", date.field = "Date")
-
-### Run analysis with the MCDS engine. Here, the WatchID is used as the sample.
-x<-distance.wrap(d,SMP_EFFORT="WatchLenKm",DISTANCE="Distance",SIZE="Count",
-                 units=list(Type="Line",Distance="Perp",Length_units="Kilometers",
-                            Distance_units="Meters",Area_units="Square kilometers"),
-                 breaks=c(0,50,100,200,300), estimator=list(c("HN","CO")),
-                 lsub=list(Alpha=c("HERG")), rare= list(Alpha=c("RBGU")), split=TRUE,
-                 STR_LABEL="STR_LABEL",STR_AREA="STR_AREA",SMP_LABEL="WatchID",
-                 path="c:/temp/distance",
-                 pathMCDS="C:/Distance 6",verbose=FALSE)
-x
-##output for the Ring-billed Gull
-summary(x)
 
