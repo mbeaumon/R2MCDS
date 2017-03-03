8ca07c47a5d294ce1f46a1d44f455f1a3393a84a
Roy <Christian.Roy@ec.gc.ca>
2017-03-03 13:38:50 -0500

    updated vignettes to reflect name change

--- vignettes/GeoAviR-covariables-models.R
+++ vignettes/GeoAviR-covariables-models.R
@@ -1,22 +1,22 @@
 ## ----warning=FALSE,message=FALSE-----------------------------------------
-library(GeoAviR)
+library(R2MCDS)
 ###set seed for reproductibility    
 set.seed(062)
 ### Import and filter data
 data(alcidae)
-alcids <- distance.filter(alcidae, transect.id = "WatchID", distance.field = "Distance", distance.labels = c("A", "B", "C", "D"), 
+alcids <- mcds.filter(alcidae, transect.id = "WatchID", distance.field = "Distance", distance.labels = c("A", "B", "C", "D"), 
                           distance.midpoints = c(25, 75, 150, 250), effort.field = "WatchLenKm", lat.field = "LatStart", 
                           long.field = "LongStart", sp.field = "Alpha", date.field = "Date")
 
 ### Run analysis with the MCDS engine. Here, the WatchID is used as the sample.
-x<-distance.wrap(alcids,SMP_EFFORT="WatchLenKm",DISTANCE="Distance",SIZE="Count",
-                 units=list(Type="Line",Distance="Perp",Length_units="Kilometers",
-                            Distance_units="Meters",Area_units="Square kilometers"),
-                 factor = c("ObserverName"), monotone = "none",
-                 breaks=c(0,50,100,200,300), SMP_LABEL="WatchID",
-                 STR_LABEL="STR_LABEL",STR_AREA="STR_AREA",
-                 path="c:/temp/distance",
-                 pathMCDS="C:/Distance 6",verbose=FALSE)
+x <- mcds.wrap(alcids,SMP_EFFORT="WatchLenKm",DISTANCE="Distance",SIZE="Count",
+                   units=list(Type="Line",Distance="Perp",Length_units="Kilometers",
+                              Distance_units="Meters",Area_units="Square kilometers"),
+                   factor = c("ObserverName"), monotone = "none",
+                   breaks=c(0,50,100,200,300), SMP_LABEL="WatchID",
+                   STR_LABEL="STR_LABEL",STR_AREA="STR_AREA",
+                   path="c:/temp/distance",
+                   pathMCDS="C:/Distance 6",verbose=FALSE)
 #Look at the output
 x
 
